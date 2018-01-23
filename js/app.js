8f4adc947a878cc32301690436e297e15b811554
Jennifer Williams <jennifer@squareandplum.com>
2018-01-22 16:23:20 -0800

    add display count and click count properties to Product object. Add names for existing Products

--- js/app.js
+++ js/app.js
@@ -1,59 +1,77 @@
 'use strict';
 
+
 //array to store all Products
 Product.allProducts = [];
+
+Product.totalClicks = 0;
+
 //make constructor for Product objects
-function Product(filepath, name) {
+function Product(filepath, name, displayCount, clickCount) {
   this.filepath = filepath;
   this.name = name;
+  this.displayCount = displayCount;
+  this.clickCount = clickCount;
   Product.allProducts.push(this);
 }
+
+
 //create instances of Products
 
 new Product('img/bag.jpg' ,'R2D2 Luggage');
-new Product('img/banana.jpg' ,'');
-new Product('img/bathroom.jpg' ,'');
-new Product('img/boots.jpg' ,'');
-new Product('img/breakfast.jpg' ,'');
-new Product('img/bubblegum.jpg' ,'');
-new Product('img/chair.jpg' ,'');
-new Product('img/cthulhu.jpg' ,'');
-new Product('img/dog-duck.jpg' ,'');
-new Product('img/dragon.jpg' ,'');
-new Product('img/pen.jpg' ,'');
-new Product('img/pet-sweep.jpg' ,'');
-new Product('img/scissors.jpg' ,'');
-new Product('img/shark.jpg' ,'');
-new Product('img/sweep.png' ,'');
-new Product('img/tauntaun.jpg' ,'');
-new Product('img/unicorn.jpg' ,'');
-new Product('img/usb.gif' ,'');
-new Product('img/water-can.jpg' ,'');
-new Product('img/wine-glass.jpg' ,'');
+new Product('img/banana.jpg' ,'Banana Slicer');
+new Product('img/bathroom.jpg' ,'Bathroom Tablet Holder');
+new Product('img/boots.jpg' ,'Toeless Boots');
+new Product('img/breakfast.jpg' ,'Breakfast Appliance');
+new Product('img/bubblegum.jpg' ,'Meatball Bubble Gum');
+new Product('img/chair.jpg' ,'Convex Chair');
+new Product('img/cthulhu.jpg' ,'Cthulhu Action Figure');
+new Product('img/dog-duck.jpg' ,'Dog Duck Bill');
+new Product('img/dragon.jpg' ,'Dragon Meat');
+new Product('img/pen.jpg' ,'Pen Utensils');
+new Product('img/pet-sweep.jpg' ,'Pet Sweeper Shoes');
+new Product('img/scissors.jpg' ,'Pizza Scissors');
+new Product('img/shark.jpg' ,'Shark Sleeping Bag');
+new Product('img/sweep.png' ,'Sweeper for Baby');
+new Product('img/tauntaun.jpg' ,'Tauntaun Sleeping Bag');
+new Product('img/unicorn.jpg' ,'Unicorn Meat');
+new Product('img/usb.gif' ,'USB Tentacle');
+new Product('img/water-can.jpg' ,'Unique Watering Can');
+new Product('img/wine-glass.jpg' ,'Decorative Wine Glass');
+
+function initializeCounts() {
+  for(var i in Product.allProducts){
+    Product.allProducts[i].displayCount = 0;
+    Product.allProducts[i].clickCount = 0;
+  }
+}
 
 
 //access the image from the DOM
-
-var imgEl = document.getElementById('img1');
-// var imgEl2 = document.getElementById('img2');
-// var imgEl3 = document.getElementById('img3');
+var imgEl1 = document.getElementById('img1');
+var imgEl2 = document.getElementById('img2');
+var imgEl3 = document.getElementById('img3');
 
 //event listener on the image
+imgEl1.addEventListener('click', randomProducts);
+imgEl2.addEventListener('click', randomProducts);
+imgEl3.addEventListener('click', randomProducts);
 
-imgEl.addEventListener('click', randomProduct);
-
-//callback function for event listener to randomly display a product image
-
-function randomProduct() {
-  //choose random Product from Product.allProducts array
-
-  var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-
-  //display the Product's image 
-
-  imgEl.src = Product.allProducts[randomIndex].filepath;
+//return a random Index from the Product.allProducts array
+function randomIndex() {
+  return Math.floor(Math.random() * Product.allProducts.length);
 }
 
-//invoke the callback on page load to show three random product images
+//callback function for event listener
 
-randomProduct();
+function randomProducts() {
+  var index = randomIndex();
+  imgEl1.src = Product.allProducts[index].filepath;
+  // Product.allProducts[index].displayCount++;
+  //imgEl2.src = Product.allProducts[index].filepath;
+  //imgEl3.src = Product.allProducts[index].filepath;
+}
+
+
+initializeCounts();
+randomProducts(0);
