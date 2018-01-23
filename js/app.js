5f97b8bfaf72f1fc3f3a1ccaa11f071e6bc177bc
Jennifer Williams <jennifer@squareandplum.com>
2018-01-23 14:37:51 -0800

    implement working display counter and click counter for three images. Very WET(Write Everything Thrice). Refactor later.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ Product.allProducts = [];
 Product.totalClicks = 0;
 
 var index1 = 0;
+var index2 = 0;
+var index3 = 0;
 
 
 //array to hold the three currently-displayed products
@@ -35,9 +37,13 @@ new Product('img/dog-duck.jpg' ,'Dog Duck Bill');
 
 //access the image from the DOM
 var imgEl1 = document.getElementById('img1');
+var imgEl2 = document.getElementById('img2');
+var imgEl3 = document.getElementById('img3');
 
 //event listener on the image
 imgEl1.addEventListener('click', countClick1);
+imgEl2.addEventListener('click', countClick2);
+imgEl3.addEventListener('click', countClick3);
 
 //return a random Index from the Product.allProducts array
 function randomIndex() {
@@ -49,17 +55,33 @@ function countClick1() {
   randomProducts();
 }
 
+function countClick2() {
+  Product.allProducts[index2].clickCount++;
+  randomProducts();
+}
+
+function countClick3() {
+  Product.allProducts[index3].clickCount++;
+  randomProducts();
+}
+
 function randomProducts() {
 
   //choose a random index
   index1 = randomIndex();
+  index2 = randomIndex();
+  index3 = randomIndex();
 
   //display that image
   imgEl1.src = Product.allProducts[index1].filepath;
+  imgEl2.src = Product.allProducts[index2].filepath;
+  imgEl3.src = Product.allProducts[index3].filepath;
 
   //increment displayCount for that index
   Product.allProducts[index1].displayCount++;
+  Product.allProducts[index2].displayCount++;
+  Product.allProducts[index3].displayCount++;
 
 }
 
-randomProducts(0);
+randomProducts();
