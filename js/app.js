b33c2072cc1b80b2fe0db874a23f0391acfcadf8
Jennifer Williams <jennifer@squareandplum.com>
2018-01-23 14:29:56 -0800

    implement working display counter and click counter for single image

--- js/app.js
+++ js/app.js
@@ -3,16 +3,19 @@
 
 Product.allProducts = [];
 Product.totalClicks = 0;
-Product.currentIndex1 = 0;
-//Product.currentIndex2 = 0;
-//Product.currentIndex3 = 0;
+
+var index1 = 0;
+
+
+//array to hold the three currently-displayed products
+//var displayedProducts = [];
 
 //make constructor for Product objects
-function Product(filepath, name, displayCount, clickCount) {
+function Product(filepath, name) {
   this.filepath = filepath;
   this.name = name;
-  this.displayCount = displayCount;
-  this.clickCount = clickCount;
+  this.displayCount = 0;
+  this.clickCount = 0;
   Product.allProducts.push(this);
 }
 
@@ -28,71 +31,35 @@ new Product('img/breakfast.jpg' ,'Breakfast Appliance');
 new Product('img/chair.jpg' ,'Convex Chair');
 // new Product('img/cthulhu.jpg' ,'Cthulhu Action Figure');
 new Product('img/dog-duck.jpg' ,'Dog Duck Bill');
-// new Product('img/dragon.jpg' ,'Dragon Meat');
-// new Product('img/pen.jpg' ,'Pen Utensils');
-// new Product('img/pet-sweep.jpg' ,'Pet Sweeper Shoes');
-// new Product('img/scissors.jpg' ,'Pizza Scissors');
-// new Product('img/shark.jpg' ,'Shark Sleeping Bag');
-// new Product('img/sweep.png' ,'Sweeper for Baby');
-// new Product('img/tauntaun.jpg' ,'Tauntaun Sleeping Bag');
-// new Product('img/unicorn.jpg' ,'Unicorn Meat');
-// new Product('img/usb.gif' ,'USB Tentacle');
-// new Product('img/water-can.jpg' ,'Unique Watering Can');
-// new Product('img/wine-glass.jpg' ,'Decorative Wine Glass');
 
-function initializeCounts() {
-  for(var i in Product.allProducts){
-    Product.allProducts[i].displayCount = 0;
-    Product.allProducts[i].clickCount = 0;
-  }
-}
-//console.log(Product.allProducts);
 
 //access the image from the DOM
 var imgEl1 = document.getElementById('img1');
-//var imgEl2 = document.getElementById('img2');
-//var imgEl3 = document.getElementById('img3');
 
 //event listener on the image
-
-imgEl1.addEventListener('click', countClick(Product.currentIndex1));
-imgEl1.addEventListener('click', randomProducts);
-//imgEl2.addEventListener('click', randomProducts);
-//imgEl3.addEventListener('click', randomProducts);
+imgEl1.addEventListener('click', countClick1);
 
 //return a random Index from the Product.allProducts array
 function randomIndex() {
   return Math.floor(Math.random() * Product.allProducts.length);
 }
 
+function countClick1() {
+  Product.allProducts[index1].clickCount++;
+  randomProducts();
+}
+
 function randomProducts() {
+
   //choose a random index
-  var index1 = randomIndex();
-
-
-  //set Product.currentIndex1 to that index
-  Product.currentIndex1 = index1;
+  index1 = randomIndex();
 
   //display that image
   imgEl1.src = Product.allProducts[index1].filepath;
 
   //increment displayCount for that index
-  //Product.allProducts[index1].displayCount++;
-
-
-  //imgEl2.src = Product.allProducts[index].filepath;
-  //imgEl3.src = Product.allProducts[index].filepath;
-
+  Product.allProducts[index1].displayCount++;
 
 }
 
-function countClick(index) {
-
-  Product.allProducts[index].clickCount++;
-  //console.log(Product.allProducts);
-  //console.log(index);
-}
-
-
-initializeCounts();
 randomProducts(0);
