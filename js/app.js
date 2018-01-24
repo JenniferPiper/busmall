9b95c1818500bdb0630889e740e183c7729f2277
Jennifer Williams <jennifer@squareandplum.com>
2018-01-23 17:29:46 -0800

    add total click count and display results in a list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 Product.allProducts = [];
 Product.totalClicks = 0;
 
@@ -8,6 +7,10 @@ var index1 = 0;
 var index2 = 0;
 var index3 = 0;
 
+//Number of total clicks before showing results.
+var maxClicks = 25;
+
+var ulEl = document.getElementById('results');
 
 //array to hold the indices of the three most-recently-displayed products
 var displayedIndices = [];
@@ -21,9 +24,7 @@ function Product(filepath, name) {
   Product.allProducts.push(this);
 }
 
-
 //create instances of Products
-
 new Product('img/bag.jpg' ,'R2D2 Luggage');
 new Product('img/banana.jpg' ,'Banana Slicer');
 new Product('img/bathroom.jpg' ,'Bathroom Tablet Holder');
@@ -33,25 +34,25 @@ new Product('img/bubblegum.jpg' ,'Meatball Bubble Gum');
 new Product('img/chair.jpg' ,'Convex Chair');
 new Product('img/cthulhu.jpg' ,'Cthulhu Action Figure');
 new Product('img/dog-duck.jpg' ,'Dog Duck Bill');
-//new Product('img/dragon.jpg' ,'Dragon Meat');
-// new Product('img/pen.jpg' ,'Pen Utensils');
-// new Product('img/pet-sweep.jpg' ,'Pet Sweeper Shoes');
-// new Product('img/scissors.jpg' ,'Pizza Scissors');
-// new Product('img/shark.jpg' ,'Shark Sleeping Bag');
-// new Product('img/sweep.png' ,'Sweeper for Baby');
-// new Product('img/tauntaun.jpg' ,'Tauntaun Sleeping Bag');
-// new Product('img/unicorn.jpg' ,'Unicorn Meat');
-// new Product('img/usb.gif' ,'USB Tentacle');
-// new Product('img/water-can.jpg' ,'Unique Watering Can');
-// new Product('img/wine-glass.jpg' ,'Decorative Wine Glass');
+new Product('img/dragon.jpg' ,'Dragon Meat');
+new Product('img/pen.jpg' ,'Pen Utensils');
+new Product('img/pet-sweep.jpg' ,'Pet Sweeper Shoes');
+new Product('img/scissors.jpg' ,'Pizza Scissors');
+new Product('img/shark.jpg' ,'Shark Sleeping Bag');
+new Product('img/sweep.png' ,'Sweeper for Baby');
+new Product('img/tauntaun.jpg' ,'Tauntaun Sleeping Bag');
+new Product('img/unicorn.jpg' ,'Unicorn Meat');
+new Product('img/usb.gif' ,'USB Tentacle');
+new Product('img/water-can.jpg' ,'Unique Watering Can');
+new Product('img/wine-glass.jpg' ,'Decorative Wine Glass');
 
 
-//access the image from the DOM
+//access the images from the DOM
 var imgEl1 = document.getElementById('img1');
 var imgEl2 = document.getElementById('img2');
 var imgEl3 = document.getElementById('img3');
 
-//event listener on the image
+//event listeners on the images
 imgEl1.addEventListener('click', countClick1);
 imgEl2.addEventListener('click', countClick2);
 imgEl3.addEventListener('click', countClick3);
@@ -63,42 +64,78 @@ function randomIndex() {
 
 function countClick1() {
   Product.allProducts[index1].clickCount++;
-  randomProducts();
+  Product.totalClicks++;
+  console.log(Product.totalClicks);
+  if(Product.totalClicks > maxClicks - 1 ){
+    imgEl1.removeEventListener('click', countClick1);
+    imgEl2.removeEventListener('click', countClick2);
+    imgEl3.removeEventListener('click', countClick3);
+    showResults();
+  }
+  else{
+    randomProducts();
+  }
 }
 
 function countClick2() {
   Product.allProducts[index2].clickCount++;
-  randomProducts();
+  Product.totalClicks++;
+  console.log(Product.totalClicks);
+  if(Product.totalClicks > maxClicks - 1){
+    imgEl1.removeEventListener('click', countClick1);
+    imgEl2.removeEventListener('click', countClick2);
+    imgEl3.removeEventListener('click', countClick3);
+    showResults();
+  }
+  else{
+    randomProducts();
+  }
 }
 
 function countClick3() {
   Product.allProducts[index3].clickCount++;
-  randomProducts();
+  Product.totalClicks++;
+  console.log(Product.totalClicks);
+  if(Product.totalClicks > maxClicks - 1 ){
+    imgEl1.removeEventListener('click', countClick1);
+    imgEl2.removeEventListener('click', countClick2);
+    imgEl3.removeEventListener('click', countClick3);
+    showResults();
+  }
+  else{
+    randomProducts();
+  }
+}
+function showResults() {
+  console.log('Results');
+  for( var i in Product.allProducts) {
+    var liEl = document.createElement('li');
+    liEl.textContent = Product.allProducts[i].name + ': Displayed ' + Product.allProducts[i].displayCount + ' times and chosen ' + Product.allProducts[i].clickCount + ' times.';
+    ulEl.appendChild(liEl);
+  }
 }
 
 function randomProducts() {
-
   //choose  random indices
-
   index1 = randomIndex();
   index2 = randomIndex();
   index3 = randomIndex();
 
   // recalculate index1 if it matches index2 or index3, or if it is found in the displayedIndices array
   while( index1 === index2 || index1 === index3 || displayedIndices.includes(index1)) {
-    console.log(index1 + '' + index2 + '' + index3);
+  // console.log(index1 + '' + index2 + '' + index3);
     index1 = randomIndex();
   }
 
   //recalculate index2 if it matches index1 or index3, or if it is found in the displayedIndices array
   while( index2 === index1 || index2 === index3 || displayedIndices.includes(index2)) {
-    console.log(index1 + '' + index2 + '' + index3);
+    //console.log(index1 + '' + index2 + '' + index3);
     index2 = randomIndex();
   }
 
   //recalculate index3 if it matches index1 or index2, or if it is found in the displayedIndices array
   while( index3 === index1 || index3 === index2 || displayedIndices.includes(index2)) {
-    console.log(index1 + '' + index2 + '' + index3);
+    //console.log(index1 + '' + index2 + '' + index3);
     index3 = randomIndex();
   }
 
@@ -118,9 +155,9 @@ function randomProducts() {
   displayedIndices.push(index2);
   displayedIndices.push(index3);
 
-  for (var j in displayedIndices) {
-    console.log(displayedIndices[j] + ': ' + Product.allProducts[displayedIndices[j]].name + ' displayed: ' + Product.allProducts[displayedIndices[j]].displayCount + ' clicked: ' + Product.allProducts[displayedIndices[j]].clickCount);
-  }
+  // for (var j in displayedIndices) {
+  //   console.log(displayedIndices[j] + ': ' + Product.allProducts[displayedIndices[j]].name + ' displayed: ' + Product.allProducts[displayedIndices[j]].displayCount + ' clicked: ' + Product.allProducts[displayedIndices[j]].clickCount);
+  // }
 
 }
 
